b5fa878f9d4912966b64df22e6ce838536cdf9aa
KashbullinRail <kashbullinrail@gmail.com>
2023-01-27 12:10:20 +0300

    2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,7 @@ fun main(args: Array<String>) {
 //    Solution1().twoSum(IntArray(6) { it * 1 }, 5)
 //    Solution1().twoSum(IntArray(2){3}, 6)
 
-
-
-
-
-
+    Solution2().isValid("(]{}[]")
 
 }
 
@@ -32,13 +28,19 @@ class Solution1 {
 
 class Solution2 {
     fun isValid(s: String): Boolean {
-        for (i in s) {
-            if (s.get(i) == "(") {
-
-            }
+        var state:Boolean = true
+      loop@ for (i in 0..s.length-1 step 2){
+            if (s[i] == '(') if (!(s[i+1] == ')')) state = false
+            if (s[i] == '[') if (!(s[i+1] == ']')) state = false
+            if (s[i] == '{') if (!(s[i+1] == '}')) state = false
+          println(state)
+          if (!state)  break@loop
+           println("${s[i]}, ${s[i+1]}")
         }
+     return state
     }
 }
+
 
 
 
